8c34e866eee413a8fb08e9ae378327ccb9f16f3b
Asadbek Rajabboyev <121626180+asadrajab28@users.noreply.github.com>
2023-09-21 22:33:30 +0500

    new commit

--- app/src/main/java/com/example/quizgamebyasadbek/fragments/FlagsFragment.java
+++ app/src/main/java/com/example/quizgamebyasadbek/fragments/FlagsFragment.java
@@ -1,8 +1,12 @@
 package com.example.quizgamebyasadbek.fragments;
 
+
+import android.graphics.drawable.Drawable;
+
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,14 +17,13 @@ import android.widget.Toast;
 
 import com.example.quizgamebyasadbek.R;
 import com.example.quizgamebyasadbek.databinding.FragmentFlagsBinding;
-import com.example.quizgamebyasadbek.models.FlagsModel;
 import com.example.quizgamebyasadbek.models.WordModel;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class FlagsFragment extends Fragment implements View.OnClickListener{
+public class FlagsFragment extends Fragment implements View.OnClickListener {
 
 
     @Override
@@ -30,17 +33,18 @@ public class FlagsFragment extends Fragment implements View.OnClickListener{
 
         }
     }
-private FragmentFlagsBinding binding;
-    private List<FlagsModel> flagsModelList;
+
+    private FragmentFlagsBinding binding;
     private int currentIndex = 0;
     private List<WordModel> wordModelList;
+    private int resourceId = R.drawable.baseline_lightbulb_24;
     private int level = 1;
     private int coins1 = 50;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding= FragmentFlagsBinding.inflate(inflater, container, false);
+        binding = FragmentFlagsBinding.inflate(inflater, container, false);
         loadData();
         setData();
         return binding.getRoot();
@@ -51,7 +55,12 @@ private FragmentFlagsBinding binding;
         binding.img.setImageResource(wordModel.getImg());
         String word = wordModel.getWord();//book
         int length = word.length();//4
+        if (wordModelList.size() <= level) {
+            Navigation.findNavController(binding.getRoot()).navigate(R.id.action_flagsFragment_to_menuFragment);
+            Toast.makeText(requireContext(), "Siz hammasini topdingiz barakalla", Toast.LENGTH_LONG).show();
+        }
         LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1.0f);
+        LinearLayout.LayoutParams layoutParams1 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 180);
 
 //        for (int i = 0; i < length; i++) {
 //            Button button = new Button(this);
@@ -80,18 +89,21 @@ private FragmentFlagsBinding binding;
 
         }
 
-        Button buttonHint =new Button(requireContext());
+        Button buttonHint = new Button(requireContext());
         buttonHint.setText("HINT");
-        buttonHint.setLayoutParams(layoutParams);
+        Drawable drawable = getResources().getDrawable(resourceId);
+        buttonHint.setCompoundDrawablesWithIntrinsicBounds(null, drawable, null, null);
+        buttonHint.setLayoutParams(layoutParams1);
+
         binding.buttonLayout.addView(buttonHint);
         buttonHint.setOnClickListener(v -> {
-            if (coins1>= 10){
+            if (coins1 >= 10) {
                 binding.textHint.setText(wordModel.getHint());
                 int i = coins1 - 10;
-                coins1=i;
+                coins1 = i;
                 buttonHint.setVisibility(View.INVISIBLE);
-                binding.coins.setText(i+"coins");
-            }else {
+                binding.coins.setText(i + "coins");
+            } else {
                 Toast.makeText(requireContext(), "Sizda yetarli mablag mavjud emas", Toast.LENGTH_SHORT).show();
             }
         });
@@ -114,11 +126,25 @@ private FragmentFlagsBinding binding;
 
     private void loadData() {
         wordModelList = new ArrayList<>();
-        wordModelList.add(new WordModel(R.drawable.img_5, "Argetina","South America"));//0
-        wordModelList.add(new WordModel(R.drawable.img_7, "Mexico","North America"));//1
-        wordModelList.add(new WordModel(R.drawable.img_6, "China", "Asia"));
-        wordModelList.add(new WordModel(R.drawable.img_4, "EGYPT", "Africa"));
-        wordModelList.add(new WordModel(R.drawable.img_3, "USA", "North America"));
+        wordModelList.add(new WordModel(R.drawable.img_5, "Argentina", "South America", 8));//0
+        wordModelList.add(new WordModel(R.drawable.img_7, "Mexico", "North America", 5));//1
+        wordModelList.add(new WordModel(R.drawable.img_6, "China", "Asia", 5));
+        wordModelList.add(new WordModel(R.drawable.img_4, "EGYPT", "Africa", 5));
+        wordModelList.add(new WordModel(R.drawable.img_3, "USA", "North America", 3));
+        wordModelList.add(new WordModel(R.drawable.img_8, "Uzbekistan", "Asia", 10));
+        wordModelList.add(new WordModel(R.drawable.img_9, "Tajikistan", "Asia", 10));
+        wordModelList.add(new WordModel(R.drawable.img_10, "India", "Asia", 5));
+        wordModelList.add(new WordModel(R.drawable.img_11, "Germany", "Europa", 7));
+        wordModelList.add(new WordModel(R.drawable.img_12, "Russia", "Europa", 6));
+        wordModelList.add(new WordModel(R.drawable.img_13, "Canada", "North America", 6));
+        wordModelList.add(new WordModel(R.drawable.img_14, "UAE", "Asia", 3));
+        wordModelList.add(new WordModel(R.drawable.img_15, "Mongolia", "Asia", 8));
+        wordModelList.add(new WordModel(R.drawable.img_16, "Kongo", "Africa", 6));
+        wordModelList.add(new WordModel(R.drawable.img_17, "Morocco", "Africa", 7));
+        wordModelList.add(new WordModel(R.drawable.img_18, "Somali", "Africa", 6));
+        wordModelList.add(new WordModel(R.drawable.img_19, "Japan", "Asia", 5));
+
+
     }
 
     @Override
@@ -161,12 +187,13 @@ private FragmentFlagsBinding binding;
                 binding.buttonLayout.removeAllViews();
                 currentIndex++;
                 level++;
-                coins1+=10;
-                binding.coins.setText(coins1+ "coins");
-                binding.textViewLevel.setText(level +"-level");
+                coins1 += 10;
+                binding.coins.setText(coins1 + "coins");
+                binding.textViewLevel.setText(level + "-level");
                 setData();
                 binding.textHint.setText("");
                 Toast.makeText(requireContext(), "Barakalla", Toast.LENGTH_SHORT).show();
+
 
             }
         }
--- app/src/main/java/com/example/quizgamebyasadbek/models/WordModel.java
+++ app/src/main/java/com/example/quizgamebyasadbek/models/WordModel.java
@@ -4,23 +4,17 @@ public class WordModel {
     private int img;
     private String word;
     private String hint;
+    private int wordSize;
 
-    public WordModel(int img, String word, String hint) {
+    public WordModel(int img, String word, String hint, int wordSize) {
         this.img = img;
         this.word = word;
         this.hint = hint;
-    }
-
-    public String getHint() {
-        return this.hint;
-    }
-
-    public void setHint(String hint) {
-        this.hint = hint;
+        this.wordSize = wordSize;
     }
 
     public int getImg() {
-        return this.img;
+        return img;
     }
 
     public void setImg(int img) {
@@ -28,10 +22,26 @@ public class WordModel {
     }
 
     public String getWord() {
-        return this.word;
+        return word;
     }
 
     public void setWord(String word) {
         this.word = word;
+    }
+
+    public String getHint() {
+        return hint;
+    }
+
+    public void setHint(String hint) {
+        this.hint = hint;
+    }
+
+    public int getWordSize() {
+        return wordSize;
+    }
+
+    public void setWordSize(int wordSize) {
+        this.wordSize = wordSize;
     }
 }
